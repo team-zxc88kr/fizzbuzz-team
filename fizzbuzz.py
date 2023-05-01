7ebe733f640bfb348b20e40d5cfd8ce62560349c
zxc88kr <zxc88kr@naver.com>
2023-05-01 18:21:49 +0900

    refactor: Do fizzbuzz with 1 if statement
    using ==, or operator

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,11 +1,7 @@
 def do_fizzbuzz(num: int):
     for i in range(1, num + 1):
-        if i % 15 == 0:
-            print('fizzbuzz')
-        elif i % 3 == 0:
-            print('fizz')
-        elif i % 5 == 0:
-            print('buzz')
+        if i % 3 == 0 or i % 5 == 0:
+            print('fizz' * (i % 3 == 0) + 'buzz' * (i % 5 == 0))
         else:
             print(i)
 
